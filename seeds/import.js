ceb758a3bd096f9ea4197cd9555881969a836623
Alexis <lxs.bnds@gmail.com>
2020-04-30 16:10:11 -0700

    csvdata pushed to array, and remove first row correctly

--- seeds/import.js
+++ seeds/import.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 const fastcsv = require("fast-csv");
 
-const stream = fs.createReadStream('routes.txt');
-
-fastcsv.parseStream(stream)
+const routeStream = fs.createReadStream('routes.txt');
+const tripsStream = fs.createReadStream('trips.txt');
+let csvData = [];
+fastcsv.parseStream(routeStream)
     .on('error', error => console.error(error))
-    .on('data', row => console.log(`ROW=${JSON.stringify(row)}`))
-    .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+    .on('data', row => {
+        // console.log(`ROW=${JSON.stringify(row)}`)
+        csvData.push(row)
+ 
+    })
+    .on('end', rowCount => {
+        // console.log(`Parsed ${rowCount} rows`)
+ 
+        csvData.shift();
+       
+    });
