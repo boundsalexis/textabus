125d1ae003d95f98b9db35750db5fefd98820e62
Alexis <lxs.bnds@gmail.com>
2020-05-02 10:36:28 -0700

    commented out the import.js file to outline what each step is doing

--- seeds/import.js
+++ seeds/import.js
@@ -1,33 +1,44 @@
+// Dependencies required for reading and parsisng csv
 const fs = require("fs");
 const fastcsv = require("fast-csv");
 
+// Connects to our DB
 const db = require("../db")
 
+// Define streams for SFMTA files
 const routeStream = fs.createReadStream('routes.txt');
 const tripsStream = fs.createReadStream('trips.txt');
 const stopsStream = fs.createReadStream('stops.txt');
 const stopTimesStream = fs.createReadStream('stop_times.txt');
+
+// Creating array to iterate through each file
+// Order DOES matter, to avoid foreign key errors
 const importFiles = [routeStream, tripsStream, stopsStream, stopTimesStream];
+
+// Creating array to hold each row of the CSV
 var csvData = [];
 
+// Do to each file
 importFiles.forEach(file => {
-    // console.log("____________________________________________________________________________________________________");
-    console.log(file.path);
-
+    //console.log(file.path)
+    // Use fastcsv to parse through csv
     fastcsv.parseStream(file)
+        // Handle errors
         .on('error', error => console.error(error))
-        .on('data', row => {
-            // console.log(`ROW=${JSON.stringify(row)}`)
+        // Handle the Data
+        .on('data', row => 
+            // Check to ensure the file is being parsed correctly
+            // console.log(`ROW=${JSON.stringify(row)}`) 
             csvData.push(row)
-       
-        })
+        )
+        //Handle the end of the data
         .on('end', rowCount => {
             // console.log(`Parsed ${rowCount} rows`)
             let query;
+            // Removes first Row table heads
             csvData.shift();
-            console.log("____________________________________________________________________________________________________");
 
-            
+            // Create querys based on the collumn heads, the values are irrelevant, simply place holders
             const routeQuery =
             "INSERT INTO routes (route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
             const tripQuery =
@@ -37,6 +48,7 @@ importFiles.forEach(file => {
             const stopTimeQuery =
             "INSERT INTO stop_times (trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign, pickup_type, drop_off_type, shape_dist_traveled) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
             
+            // switch to relevant query depending on what file we are on 
             if (file.path==="routes.txt"){
                 query= routeQuery;
             }else if(file.path ==="trips.txt"){
@@ -46,15 +58,15 @@ importFiles.forEach(file => {
             }else if (file.path === "stop_times.txt"){
                 query=stopTimeQuery;
             }
-
+            // go through each row
             csvData.forEach(row => {
-
+                // Insert each row into DB
                 db.query(query, row, (err, res) => {
                     if (err) {
-                        console.log(err);
+                        console.log(err.stack);
                     }
                     else {
-                        console.log("res")
+                        console.log(res)
                     }
 
                 })
